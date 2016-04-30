92fa47b736c2cd89f3f6efb8b050616b7be660cb
Rob Hardwick <robhardwick@gmail.com>
2016-04-30 15:33:34 +0100

    Ensure "pv" exists in carbon-copy script

--- carbon-copy.sh
+++ carbon-copy.sh
@@ -1,16 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
+# Ensure "pv" exists
+if ! hash pv 2>/dev/null; then
+    printf "pv not found. Try installing it with:\n\tbrew install pv\n\n";
+
 # Check args
-if [ "$#" -lt 2 ]; then
-    printf "Usage : ${0} <input file/devices> <output file/device> [<block size>]\n\n";
-    exit;
-fi
+elif [ "${#}" -lt 2 ]; then
+    PROG=$(basename "${0}")
+    printf "Usage: ${PROG} <input device> <output file> [<block size>]\n\n";
 
 # Run
-# 1. Assume input (arg 1) is a disk device node or similar (which requires root)
-# 2. Assume output (arg 2) is writable by the current user
-pv "${1}" | dd of="${2}" bs="${3:-64k}" conv="notrunc"
+else
+    # 1. Assume input (arg 1) is a disk device node or similar (which requires root)
+    # 2. Assume output (arg 2) is writable by the current user
+    sudo pv "${1}" | dd of="${2}" bs="${3:-64k}" conv="notrunc"
 
-# Force any pending writes
-sync
+    # Force any pending writes
+    sync
+fi
